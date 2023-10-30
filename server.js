4ca92f87b0d780038828ef65b5baf6c8f50357d2
coder-Rit <001patilritesh@gmail.com>
2023-10-31 04:12:45 +0530

    all time high fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+const schedule = require('node-schedule');
 
 const app = require('./app.js');
 
@@ -9,3 +10,8 @@ const app = require('./app.js');
 app.listen(process.env.PORT,()=>{
     console.log("wlc to ",process.env.PORT);
 })  
+
+const job = schedule.scheduleJob(' */13 * * * *', function async(){
+    axios.post(" ").then(data=>console.log(data.data.msg));
+  });
+  
